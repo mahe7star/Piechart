802252596762f36c8f39e1c2bcbcb9e758ef1347
Mahesh Pratap <maheshpratap@MacBook-Air-2.local>
2018-12-30 23:08:54 +0530

    minor fix

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ const circle1 = document.getElementById('circle1');
 const peiPercentage = document.getElementById('peiPercentage');
 
 peiPercentage.addEventListener('input', () => {
+  
   let percentageInput = peiPercentage.value;
-  circle1.style.strokeDasharray = `${percentageInput} 100`;
+  if(percentageInput >= 100) {
+    percentageInput.readonly = true;
+    alert('percentage cannot be greater then 100!')
+  } else {
+    percentageInput.readonly = false;
+    circle1.style.strokeDasharray = `${percentageInput} 100`;
+  }
 });
